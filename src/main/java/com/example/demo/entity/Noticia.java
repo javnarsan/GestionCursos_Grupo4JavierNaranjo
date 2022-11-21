42ab56eca5b558f4a4e37c9d932f2e3483f707f3
Alejandro Junquera <junquera.maale21@cadiz.salesianos.edu>
2022-11-21 12:07:31 +0100

    Completar entidad noticia y profesor

--- src/main/java/com/example/demo/entity/Noticia.java
+++ src/main/java/com/example/demo/entity/Noticia.java
@@ -1,5 +1,45 @@
 package com.example.demo.entity;
 
 public class Noticia {
-
+	private int id;
+	private String titulo;
+	private String descripcion;
+	private String imagen;
+	private int IdAdministrador;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getImagen() {
+		return imagen;
+	}
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+	public int getIdAdministrador() {
+		return IdAdministrador;
+	}
+	public void setIdAdministrador(int idAdministrador) {
+		IdAdministrador = idAdministrador;
+	}
+	@Override
+	public String toString() {
+		return "Noticia [id=" + id + ", titulo=" + titulo + ", descripcion=" + descripcion + ", imagen=" + imagen
+				+ ", IdAdministrador=" + IdAdministrador + "]";
+	}
+	
 }
--- src/main/java/com/example/demo/entity/Profesor.java
+++ src/main/java/com/example/demo/entity/Profesor.java
@@ -1,5 +1,52 @@
 package com.example.demo.entity;
 
 public class Profesor {
-
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String usuario;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Profesor [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
+				+ ", usuario=" + usuario + ", password=" + password + "]";
+	}
+	
 }
