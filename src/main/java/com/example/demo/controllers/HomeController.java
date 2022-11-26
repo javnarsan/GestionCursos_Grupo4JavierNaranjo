76b9a0f95710d17e18aeb5934b66d4b39f0cb82b
cocohuevo <48055965+cocohuevo@users.noreply.github.com>
2022-11-26 16:28:42 +0100

    Plantilla base usando webjars y thymeleaf utilizando layouts globales

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+	
+	@GetMapping({"/","inicio"})
+	public String insertarTituloPestaña(Model model) {
+		String titulo="inicio";
+		model.addAttribute("titulo",titulo);
+		return "index.html";
+	}
+
+}
