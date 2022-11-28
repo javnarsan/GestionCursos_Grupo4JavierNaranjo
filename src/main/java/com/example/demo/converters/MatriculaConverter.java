b14bf71fa0a12546a47d41375667d7bdc3eceb99
cocohuevo <48055965+cocohuevo@users.noreply.github.com>
2022-11-28 18:51:07 +0100

    Líneas comentadas 2

--- src/main/java/com/example/demo/converters/MatriculaConverter.java
+++ src/main/java/com/example/demo/converters/MatriculaConverter.java
@@ -11,16 +11,16 @@ public class MatriculaConverter {
 	public MatriculaModel entity2model(Matricula matricula) {
 		MatriculaModel matriculaModel=new MatriculaModel();
 		matriculaModel.setId(matricula.getId());
-		matriculaModel.setDdAlumno(matricula.getDdAlumno());
-		matriculaModel.setIdCurso(matricula.getIdCurso());
+		//matriculaModel.setDdAlumno(matricula.getDdAlumno());
+		//matriculaModel.setIdCurso(matricula.getIdCurso());
 		matriculaModel.setValoraciones(matricula.getValoraciones());
 		return matriculaModel;
 	}
 	public Matricula model2entity(MatriculaModel matriculaModel) {
 		Matricula matricula=new Matricula();
 		matricula.setId(matriculaModel.getId());
-		matricula.setDdAlumno(matriculaModel.getDdAlumno());
-		matricula.setIdCurso(matriculaModel.getIdCurso());
+		//matricula.setDdAlumno(matriculaModel.getDdAlumno());
+		//matricula.setIdCurso(matriculaModel.getIdCurso());
 		matricula.setValoraciones(matriculaModel.getValoraciones());
 		return matricula;
 		
